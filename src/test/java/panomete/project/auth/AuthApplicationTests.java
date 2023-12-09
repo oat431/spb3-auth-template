054cb8513cbe6601d56d034df3cc411387c74d5a
oat431 <oat431@gmail.com>
2023-12-10 01:42:17 +0700

    🛠 setup : jwt auth template

---
+++ src/test/java/panomete/project/auth/AuthApplicationTests.java
@@ -0,0 +1,13 @@
+package panomete.project.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
